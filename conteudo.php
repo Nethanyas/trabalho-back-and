e1a355ee6a0b22b4791566b9ddede9b9c37780b7
Nethanyas <nethanyasdomingosdasilva@gmail.com>
2025-11-07 17:19:51 -0300

    conteudo.php criado

---
+++ conteudo.php
@@ -0,0 +1,23 @@
+<!-- exibição de conteúdo-->
+<div id="conteudo"></div>
+<!-- caixa de exclusão -->
+    <div id="excluir_contato_modal" class="modal fade" rabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"
+                    aria-label="fechar">
+                    <span aria-hidden="true">$time;</span>
+                </button>
+                <h4 class="modal-tritle">excluir contato</h4>
+            </div>
+            <div class="modal-body">
+                <p>Confirma a exclus&atilde;o do contato?</p>
+            </div>
+            <div class="modal-footer">
+                <input type="hidden" id="contato_id" value="" />
+                <button type="button" id="excluir" class="btn-btn_primary">Sim</button>
+                <button type="button" class="btn btn-default" data-dismiss="modal">N&atilde;o</button>
+            </div>
+        </div>
+    </div>
+</div>
