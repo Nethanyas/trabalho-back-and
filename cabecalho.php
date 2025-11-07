7865ba96afbb7dd977202a9194eaa93a0aa1d4a3
Nethanyas <nethanyasdomingosdasilva@gmail.com>
2025-11-07 18:10:14 -0300

    concerto no css

--- cabecalho.php
+++ cabecalho.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRUD Contato - PHP Profissional</title>
-    <link href="./agenda/recursos/bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="./agenda/recursos/common/css/bootstrap.css" rel="stylesheet">
 </head>
 <body>
     <div class="container">
